48b39df030c3e4dd64d9277ced075fbc7fa6c460
realdinho <realdo.dias3@gmail.com>
2023-07-07 22:36:22 +0800

    setup docker compose for dev, prod, and default envs

--- src/main/java/com/realdiv/accounts/AccountsApplication.java
+++ src/main/java/com/realdiv/accounts/AccountsApplication.java
@@ -2,8 +2,17 @@ package com.realdiv.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+// import org.springframework.boot.autoconfigure.domain.EntityScan;
+// import org.springframework.context.annotation.ComponentScan;
+// import org.springframework.context.annotation.ComponentScans;
+// import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
+// @ComponentScans({ @ComponentScan("com.realdiv.accounts.controller") })
+// @EnableJpaRepositories("com.realdiv.accounts.repository")
+// @EntityScan("com.realdiv.accounts.model")
 public class AccountsApplication {
 
 	public static void main(String[] args) {
